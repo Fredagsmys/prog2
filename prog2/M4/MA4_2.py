f55eaa4a93d0401cbeb7f0f016f24bdf5479b55c
Fredagsmys <max.mattsson00@gmail.com>
2022-10-08 12:45:53 +0200

    Added fib function for MA4_2

--- prog2/M4/MA4_2.py
+++ prog2/M4/MA4_2.py
@@ -3,21 +3,23 @@
 from person import Person
 
 
-def fib(n):
-    print("hej")
-    if n <= 1:
-        return n
+def fib(_val):
+    cache = {0: 0, 1: 1}
 
-    else:
-        return fib(n - 1) + fib(n - 2)
+    def _fib(val):
+        if val in cache:
+            return cache.get(val)
 
+        cache[val] = _fib(val - 1) + _fib(val - 2)
+        return cache[val]
 
+    return _fib(_val)
 
 
 def main():
     f = Person(20)
     print(f.get())
-    f.set(15)
+    f.set(25)
     print(f.get())
     print(fib(f.get()))
 
