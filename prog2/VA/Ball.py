3bf3e323141847dc4b931e0b12c142f16cd58f9b
Max <max.matttsson00@gmail.com>
2022-10-11 01:09:22 +0200

    made balls bounce at corners

--- prog2/VA/Ball.py
+++ prog2/VA/Ball.py
@@ -1,10 +1,41 @@
 import numpy as np
+
+
 class Ball:
 
-    def __init__(self, size, spd_x, spd_y, pos_x, pos_y, color):
-        self.size = size
-        self.speed = np.array(spd_x, spd_y)
-        self.pos = np.array(pos_x, pos_y)
+    def __init__(self, radius, spd_x, spd_y, pos_x, pos_y, color):
+        self.radius = radius
+        self.speed = np.array([spd_x, spd_y])
+        self.pos = np.array([pos_x, pos_y])
         self.color = color
+        self.object = None
 
+    def get_pos_x(self):
+        return self.pos[0]
 
+    def get_pos_y(self):
+        return self.pos[1]
+
+    def get_spd_x(self):
+        return self.speed[0]
+
+    def get_spd_y(self):
+        return self.speed[1]
+
+    def set_pos_x(self, pos):
+        self.pos[0] = pos
+
+    def set_pos_y(self, pos):
+        self.pos[1] = pos
+
+    def set_spd_x(self, spd):
+        self.speed[0] = spd
+
+    def set_spd_y(self, spd):
+        self.speed[1] = spd
+
+    def set_obj(self, obj):
+        self.object = obj
+
+    def get_obj(self):
+        return self.object
--- prog2/VA/Simulation.py
+++ prog2/VA/Simulation.py
@@ -1,35 +1,86 @@
 import tkinter as tk
 import Ball
+import random as rand
+
 
 class Simulation:
 
     def __init__(self, balls):
+        self.started = False
+        self.timestep = 1
+        self.speed = 1
+        self.width = 1000
+        self.height = 800
         self.gui = tk.Tk()
-        self.canvas = tk.Canvas(self.gui, width=1000, height=800, borderwidth=0, highlightthickness=0, bg="black")
-        self.time = 0
-        self.balls = [Ball.Ball(10, 10, 10) for _ in range(balls)]
+        self.canvas = tk.Canvas(self.gui, width=self.width, height=self.height, borderwidth=0, highlightthickness=0,
+                                bg="black")
 
-    def __str__(self):
-        pass
+        self.time = 0
+        rad = 20
+        self.balls = [Ball.Ball(radius=rad * (rand.random() + 1),
+                                spd_x=self.speed * (1+rand.random()),
+                                spd_y=self.speed * (1+rand.random()),
+                                pos_x=rand.randint(rad, self.width - rad),
+                                pos_y=rand.randint(rad, self.height - rad),
+                                color='blue') for _ in range(balls)]
+        self.start()
+        self.canvas.mainloop()
 
     def start(self):
+        self.started = True
         self.canvas.grid()
 
-        self.draw_circle()
-        self.gui.mainloop()
+        self.spawn_obj()
+        self.animate()
+        self.step()
 
-    def draw_circle(self):
+    def animate(self):
+        if self.started:
+            self.step()
+            self.update()
+            self.canvas.after(1, self.animate)
+
+    def update(self):
+
         for ball in self.balls:
-            self.canvas.create_oval(100, 100, 200, 200, fill='blue')
+            rad = ball.radius
+            self.canvas.coords(ball.get_obj(), ball.get_pos_x() - rad, ball.get_pos_y() - rad, ball.get_pos_x() + rad,
+                               ball.get_pos_y() + rad)
+            coords = self.canvas.coords(ball.get_obj())
+            if coords[0] <= 0:
+                ball.set_spd_x(-ball.get_spd_x())
+
+            elif coords[0] + 2 * rad >= self.width:
+                ball.set_spd_x(-abs(ball.get_spd_x()))
+
+            elif coords[1] <= 0:
+                ball.set_spd_y(abs(ball.get_spd_y()))
+
+            elif coords[1] + 2 * rad >= self.height:
+                ball.set_spd_y(-abs(ball.get_spd_y()))
+
+    def spawn_obj(self):
+        for ball in self.balls:
+            ball.set_obj(self.canvas.create_oval(ball.pos[0] - ball.radius,
+                                                 ball.pos[1] - ball.radius,
+                                                 ball.pos[0] + ball.radius,
+                                                 ball.pos[1] + ball.radius,
+                                                 fill='blue'))
 
     def step(self):
         self.time += 1
 
+        #     for key in self.balls.keys():
+        for ball in self.balls:
+            # ball = self.balls.get(key)
+            ball.set_pos_x(ball.get_pos_x() + self.timestep * ball.get_spd_x())
+            ball.set_pos_y(ball.get_pos_y() + self.timestep * ball.get_spd_y())
+
+
 def main():
+    sim = Simulation(3)
 
-    sim = Simulation()
-
-    sim.start()
+    # sim.start()
 
 
 if __name__ == "__main__":
