fec28cba64773e79010c1d15d2c38d57b9e155e9
Maximilian Mattsson <mama9867@arrhenius.it.uu.se>
2022-10-10 14:56:00 +0200

    ny upplaga

--- prog2/M4/MA4_2.py
+++ prog2/M4/MA4_2.py
@@ -2,7 +2,7 @@
 
 from person import Person
 from numba import njit
-
+from time import perf_counter as toc
 
 @njit
 def fib_numba(val):
@@ -26,13 +26,21 @@ def fib(val):
 def main():
     f = Person(20)
     print(f.get())
-    f.set(25)
+    f.set(35)
     print(f.get())
     age = f.get()
+    start = toc()
     print("python fib of", age, "is", fib(age))
+    end = toc()
+    print("time for python:",end-start,"s")
+    start = toc()
     print("numba fib of", age, "is", fib_numba(age))
+    end = toc()
+    print("time for numba:",end-start,"s")
+    start = toc()
     print("c++ fib of", age, "is", f.fib())
-
+    end = toc()
+    print("time for c++:",end-start,"s")
 
 if __name__ == '__main__':
     main()
