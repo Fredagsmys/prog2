1f727211197adcd449e3ab7cfb7b4a4b34142050
Max <max.matttsson00@gmail.com>
2022-10-12 17:28:55 +0200

    tried adding collision but there are bugs

--- prog2/VA/Simulation.py
+++ prog2/VA/Simulation.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+
+import numpy
+
 import Ball
 import random as rand
 import tkinter.ttk as ttk
@@ -28,7 +31,7 @@ class Simulation:
         self.gui.started.set(True)
         self.gui.speed.set(1)
         self.gui.paused.set(False)
-        self.gui.balls.set(5)
+        self.gui.balls.set(2)
         balls_entry.pack()
         start_entry.pack()
         speed_entry.pack()
@@ -49,8 +52,8 @@ class Simulation:
 
     def animate(self):
         if not self.gui.paused.get() and self.gui.started.get():  # if sim is started and not paused
-            self.step()
             self.update()
+            self.step()
             self.gui.canvas.after(1, self.animate)
         elif self.gui.paused.get():  # if sim is paused
             self.gui.canvas.after(1, self.animate)
@@ -60,25 +63,37 @@ class Simulation:
     def update(self):
         for ball in self.balls:
             rad = ball.radius
+
             self.gui.canvas.coords(ball.get_obj(), ball.get_pos_x() - rad, ball.get_pos_y() - rad,
                                    ball.get_pos_x() + rad,
                                    ball.get_pos_y() + rad)
 
+            rad = ball.radius
+
             coords = self.gui.canvas.coords(ball.get_obj())
 
             collision = list(self.gui.canvas.find_overlapping(coords[0], coords[1], coords[2], coords[3]))
-            collision.remove(ball.get_obj())
-
-            if len(collision) != 0:
+            #find which ball is the one in the current loop iteration. Then set that one to ball1. And then we update this balls speed and not the one that ball1 is colliding with.
+            if len(collision) == 2:
+                updates = []  # assume this will have size 2
                 for coll in collision:
-                    self.gui.canvas.delete(coll)
-                    for ball in self.balls:
-                        if ball.get_obj() == coll:
-                            self.balls.remove(ball)
+                    for update_ball in self.balls:
+                        if update_ball.get_obj() == coll:
+                            updates.append(update_ball)
 
+                ball1 = updates[0] # change so that ball1 becomes the ball in the current iteration of for ball in self.balls
+                ball2 = updates[1]
+                print(len(updates))
+                print(numpy.dot(numpy.dot(ball2.speed - ball1.speed, ball2.pos - ball1.pos) / (
+                        numpy.absolute(ball2.pos - ball1.pos) ** 2), (ball2.pos - ball1.pos)))
 
+                ball1.speed += (numpy.dot(ball2.speed - ball1.speed, ball2.pos - ball1.pos) / (
+                        numpy.absolute(ball2.pos - ball1.pos) ** 2)) * (ball2.pos - ball1.pos)
+                print("ball1", ball1.speed)
+                # ball2.speed += (numpy.dot(ball1.speed - ball2.speed, ball2.pos - ball1.pos) / (
+                # numpy.absolute(ball2.pos - ball1.pos) ** 2)) * (ball2.pos - ball1.pos)
 
-            if coords[0] <= 0:
+            elif coords[0] <= 0:
                 ball.set_spd_x(-ball.get_spd_x())
 
             elif coords[0] + 2 * rad >= self.width:
@@ -89,6 +104,14 @@ class Simulation:
 
             elif coords[1] + 2 * rad >= self.height:
                 ball.set_spd_y(-abs(ball.get_spd_y()))
+
+            #            collision.remove(ball.get_obj())
+            #            if len(collision) != 0:
+            #                for coll in collision:
+            #                    self.gui.canvas.delete(coll)
+            #                    for delball in self.balls:
+            #                        if delball.get_obj() == coll:
+            #                            self.balls.remove(delball)
 
 
 
@@ -101,8 +124,10 @@ class Simulation:
         rad = 20
 
         self.balls = [Ball.Ball(radius=rad * (rand.random() + 1),
-                                spd_x=(1 + 4*rand.random()/5),
-                                spd_y=(1 + 4*rand.random()/5),
+                                #spd_x=(1 + 4 * rand.random() / 5),
+                                #spd_y=(1 + 4 * rand.random() / 5),
+                                spd_x=float(1),
+                                spd_y=float(1),
                                 pos_x=rand.randint(rad, self.width - rad),
                                 pos_y=rand.randint(rad, self.height - rad),
                                 color='blue') for _ in range(self.gui.balls.get())]
@@ -117,9 +142,8 @@ class Simulation:
         self.time += 1
 
         #     for key in self.balls.keys():
-        for ball in self.balls:
-            # ball = self.balls.get(key)
 
+        for ball in self.balls:
             ball.set_pos_x(ball.get_pos_x() + self.gui.curr_speed * self.timestep * ball.get_spd_x())
             ball.set_pos_y(ball.get_pos_y() + self.gui.curr_speed * self.timestep * ball.get_spd_y())
 
