0e69fa7c2512f7e25bef310f21ce4c7eb8317676
Fredagsmys <max.mattsson00@gmail.com>
2022-10-14 08:51:43 +0200

    small changes from laptop

--- prog2/VA/Simulation.py
+++ prog2/VA/Simulation.py
@@ -7,6 +7,13 @@ import random as rand
 import tkinter.ttk as ttk
 
 
+'''
+Reviewed by: Mikael Johansson
+Name: Max Mattsson
+Date: 2022-10-14
+mail: max.mattsson00@gmail.com
+'''
+
 class Simulation:
 
     def __init__(self):
@@ -15,11 +22,12 @@ class Simulation:
         self.gui.paused = tk.BooleanVar()
         self.gui.started = tk.BooleanVar()
         self.gui.balls = tk.IntVar()
-        self.gui.speed = tk.DoubleVar()
+        self.gui.speed = tk.IntVar()
         self.timestep = 0.01
         self.width = 1000
         self.height = 700
         self.time = 0
+        self.max_speed = 100
 
         self.gui.canvas = tk.Canvas(self.gui, width=self.width, height=self.height, borderwidth=0, highlightthickness=0,
                                     bg="black")
@@ -47,19 +55,24 @@ class Simulation:
         if self.gui.started.get():
             self.clear_obj()
             self.spawn_obj()
-            self.gui.curr_speed = self.gui.speed.get()
+            if self.gui.speed.get() <= self.max_speed:
+                self.gui.curr_speed = self.gui.speed.get()
+            else:
+                self.gui.curr_speed = self.max_speed
+                self.gui.speed.set(self.max_speed)
+
         self.animate()
 
     def animate(self):
         if not self.gui.paused.get() and self.gui.started.get():  # if sim is started and not paused
-            self.move_balls()   # update positions on canvas
-            self.update()       # calculate new speeds
-            self.step()         # update the position of the balls
-            self.gui.canvas.after(1, self.animate)
+            self.move_balls()  # update positions on canvas
+            self.update()  # calculate new speeds
+            self.step()  # update the position of the balls
+            self.gui.canvas.after(self.max_speed//self.gui.curr_speed, self.animate)
         elif self.gui.paused.get():  # if sim is paused
-            self.gui.canvas.after(1, self.animate)
+            self.gui.canvas.after(self.max_speed//self.gui.curr_speed, self.animate)
         else:  # if sim is stopped
-            self.gui.canvas.after(1, self.start)
+            self.gui.canvas.after(self.max_speed//self.gui.curr_speed, self.start)
 
     def move_balls(self):
         for ball in self.balls:
@@ -84,12 +97,9 @@ class Simulation:
             elif coords[1] + 2 * rad >= self.height:
                 ball.set_spd_y(-abs(ball.get_spd_y()))
 
-
             collision = list(self.gui.canvas.find_overlapping(coords[0], coords[1], coords[2], coords[3]))
             if len(collision) == 2:
                 prob = rand.random()
-
-
 
                 ball1 = None
                 ball2 = None
@@ -100,7 +110,6 @@ class Simulation:
                         if b.get_obj() == c and b != ball:
                             ball2 = b
 
-
                 if not ball1.updated and not ball2.updated:
                     if prob > 0.998:
                         todelete.append(ball1)
@@ -108,22 +117,23 @@ class Simulation:
                     ball1.updated = True
                     ball2.updated = True
 
-
                     b1_spd = ball1.speed
                     b2_spd = ball2.speed
                     ball1.speed = ball1.speed + (numpy.dot(b2_spd - b1_spd, ball2.pos - ball1.pos) / (
-                                numpy.dot(numpy.absolute(ball2.pos - ball1.pos),numpy.absolute(ball2.pos - ball1.pos)))) * (ball2.pos - ball1.pos)
+                        numpy.dot(numpy.absolute(ball2.pos - ball1.pos), numpy.absolute(ball2.pos - ball1.pos)))) * (
+                                              ball2.pos - ball1.pos)
 
                     ball2.speed = ball2.speed + (numpy.dot(b1_spd - b2_spd, ball2.pos - ball1.pos) / (
-                                numpy.dot(numpy.absolute(ball2.pos - ball1.pos),numpy.absolute(ball2.pos - ball1.pos)))) * (ball2.pos - ball1.pos)
+                        numpy.dot(numpy.absolute(ball2.pos - ball1.pos), numpy.absolute(ball2.pos - ball1.pos)))) * (
+                                              ball2.pos - ball1.pos)
 
-        for ball in self.balls: # set all update flags to false
-            for delbal in todelete:
-                if ball.get_obj() == delbal.get_obj():
-                    self.balls.remove(delbal)
-                    self.gui.canvas.delete(ball.get_obj())
+        for ball in self.balls:  # set all update flags to false
+
             ball.updated = False
 
+        for delbal in todelete:
+                self.balls.remove(delbal)
+                self.gui.canvas.delete(delbal.get_obj())
 
     def clear_obj(self):
         if self.balls is not None:
@@ -132,7 +142,8 @@ class Simulation:
 
     def spawn_obj(self):
         rad = 20
-        colors = ['blue', 'red', 'yellow', 'green', 'white', 'gold','blue', 'red', 'yellow', 'green', 'white', 'gold','blue', 'red', 'yellow', 'green', 'white', 'gold','blue', 'red', 'yellow', 'green', 'white', 'gold']
+        colors = ['blue', 'red', 'yellow', 'green', 'white', 'gold', 'blue', 'red', 'yellow', 'green', 'white', 'gold',
+                  'blue', 'red', 'yellow', 'green', 'white', 'gold', 'blue', 'red', 'yellow', 'green', 'white', 'gold']
         self.balls = [Ball.Ball(radius=rad * (rand.random() + 1),
                                 spd_x=1 + 4 * rand.random(),
                                 spd_y=1 + 4 * rand.random(),
@@ -149,7 +160,8 @@ class Simulation:
     def step(self):
         self.time += 1
         for ball in self.balls:
-            ball.pos += self.gui.curr_speed * self.timestep * ball.speed
+            ball.pos += 100 * self.timestep * ball.speed
+
 
 def main():
     Simulation()
